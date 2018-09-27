c7a48aafd790a3cd556eaa37826759c355f154af
Cecilia Assis <cy_assis@hotmail.com>
2018-09-27 00:30:56 -0300

    Junção testes de execução

--- test_cliente.py
+++ test_cliente.py
@@ -9,11 +9,11 @@ class TestCliente:
     client = pexpect.spawn('python3 cliente.py')
 
     p_id    = 'value-ne'
-    rand_id = 0
+    rand_id = 1
     regex   = r'[^N]Ok'
 
     if process_id:
-      rand_id = random.randint(0, 999999999)
+      rand_id = random.randint(3, 999999999)
       p_id    = process_id
       
     client.sendline('create {} old-{}'.format(rand_id, p_id))
@@ -40,11 +40,11 @@ class TestCliente:
     client = pexpect.spawn('python3 cliente.py')
 
     p_id    = 'value-ne'
-    rand_id = 1
+    rand_id = 2
     regex   = r'NOk'
 
     if process_id:
-      rand_id = random.randint(0, 999999999)
+      rand_id = random.randint(3, 999999999)
       p_id    = process_id
 
     client.sendline('create {} {}'.format(rand_id, p_id))
@@ -67,9 +67,35 @@ class TestCliente:
 
     client.kill(9)
 
+  '''
+    Teste iteração
+  '''
+  def execution(self, process_id = None):
+    client  = pexpect.spawn('python3 cliente.py')
+    rand_id = random.randint(3, 999999999) if process_id != None else 0
+
+    client.sendline('create {} 1\n'.format(rand_id))
+    client.sendline()
+
+    for i in range(rand_id + 1, rand_id + 1001):
+      client.sendline('read {}'.format(i - 1))
+      client.expect(r'Ok - Item: Chave: \d*, Valor: (\d*)')
+      v = int(client.match.group(1).decode())
+      client.sendline('\r\ncreate {} {}\r\n'.format(i, v + 1))
+
+    client.sendline('read {}'.format(i))
+    client.expect(r'Ok - Item: Chave: \d*, Valor: (\d*)')
+    v = int(client.match.group(1).decode())
+    assert v == 1001
+
+  def clear_log(self):
+    pexpect.spawn('rm logs.log')
+
   def all(self, process_id = None):
-    self.crud_ok(process_id)
-    self.crud_nok(process_id)
+    # self.crud_ok(process_id)
+    # self.crud_nok(process_id)
+    # self.clear_log()
+    self.execution(process_id)
 
   def test_sequencitial(self):
     self.all()
