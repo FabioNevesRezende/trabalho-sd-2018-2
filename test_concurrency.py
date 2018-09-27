8935600662a35858e859333d0aa69cab0265fa24
Cecilia Assis <cy_assis@hotmail.com>
2018-09-26 23:19:44 -0300

    Utilização de subprocessos para test concorrente

--- test_concurrency.py
+++ test_concurrency.py
@@ -1,47 +1,19 @@
 import pexpect
 import random
-import string
-from threading import *
+import threading
+from concurrent import futures
 
-class TesteConcurrency:
-  @classmethod
-  def setup_class(self):
-    self.client_instances = []
-    self.client_amount = 100
-    self.test_startServer()
-    
-  @classmethod
-  def test_startServer(self):
-    self.server = pexpect.spawn('python3 servidor.py')
-    for i in range(0, self.client_amount):
-      c = pexpect.spawn('python3 cliente.py')
-      client_instances.append(c)
-        
-  # gera uma string aleatória de tamanho <size>
-  @classmethod    
-  def str_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return ''.join(random.choice(chars) for _ in range(size))
+client_amount = 10
 
-  @classmethod
-  def client_send_data(c):
-    c.sendline('create ' + str(random.randint(0,999999999)) + ' ' + self.str_generator(random.randint(0,3000)) + '\r\n')
-    
-  @classmethod   
-  def client_thread(c):
-    rolls = 10000000
-    while rolls > 0:
-        self.client_send_data(c)
-        rolls -= 1
-        
-  # inicia uma thread para cada cliente enviar dados de forma concorrente
-  def testa(self):
-      for c in self.client_instances:
-        fio = Thread(target=self.client_thread, args=(c,))
-        fio.daemon = True
-        fio.start()
-        
-        
-            
-      
-  
-        
+def create_item():
+  client = pexpect.spawn('python3 cliente.py')
+  client.sendline('create {} vla\n\n'.format(random.randint(0, 222222)))
+  client.expect('Ok')
+
+# inicia uma thread para cada cliente enviar dados de forma concorrente
+def main():
+  with futures.ProcessPoolExecutor(max_workers=client_amount) as executor:
+    [executor.submit(create_item) for i in range(client_amount)]
+
+if __name__ == '__main__':
+  main()
